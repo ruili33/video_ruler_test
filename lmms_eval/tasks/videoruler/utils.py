be4599283d1cef1e4a1e4691010310d00a45f848
Rui Li <ruili@pasteur7.stanford.edu>
2025-06-07 03:05:12 -0700

    initial commirt

--- lmms_eval/tasks/videoruler/utils.py
+++ lmms_eval/tasks/videoruler/utils.py
@@ -52,10 +52,10 @@ def convert_time_to_frame(time_in_seconds, fps):
 
 
 
-def videomme_doc_to_visual(doc):
+def videoruler_doc_to_visual(doc):
     cache_dir = os.path.join(base_cache_dir, cache_name)
-    video_path = doc["videoID"] + ".mp4"
-    video_path = os.path.join(cache_dir, "data", video_path)
+    video_path = doc["video"] 
+    video_path = os.path.join(cache_dir, video_path)
     if os.path.exists(video_path):
         video_path = video_path
     elif os.path.exists(video_path.replace("mp4", "MP4")):
@@ -67,14 +67,6 @@ def videomme_doc_to_visual(doc):
     return [video_path]
 
 
-def videomme_doc_to_text(doc, lmms_eval_specific_kwargs=None):
-    option_prompt = "Select the best answer to the following multiple-choice question based on the video and the subtitles. Respond with only the letter (A, B, C, or D) of the correct option."
-    question = doc["question"]
-    option = "\n".join([f"{opt}" for i, opt in enumerate(doc["options"])])
-    question = question + "\n" + option
-    post_prompt = lmms_eval_specific_kwargs["post_prompt"] if "post_prompt" in lmms_eval_specific_kwargs else "The best answer is:"
-    full_prompt = option_prompt + "\n" + question + "\n" + post_prompt
-    return full_prompt
 
 
 # Frames + Subs
@@ -90,61 +82,6 @@ def videomme_doc_to_text(doc, lmms_eval_specific_kwargs=None):
 # The best answer is:
 
 
-def videomme_doc_to_text_subtitle(doc, lmms_eval_specific_kwargs=None):
-    cache_dir = os.path.join(base_cache_dir, cache_name)
-    video_path = doc["videoID"] + ".mp4"
-    video_path = os.path.join(cache_dir, "data", video_path)
-    subtitle_path = os.path.join(cache_dir, "subtitle", doc["videoID"] + ".srt")
-    video_path = os.path.join(cache_dir, video_path)
-    if os.path.exists(subtitle_path):  # Denote have subtitle
-        subtitle = open(subtitle_path).readlines()
-    else:
-        subtitle = ""
-    subtitles_prompt = "This video's subtitles are listed below: \n"
-    if subtitle == "":
-        subtitle = "No subtitles available"
-    else:
-        if "gemini_api_flag" in lmms_eval_specific_kwargs:  # specific for gemini_api
-            if lmms_eval_specific_kwargs["gemini_api_flag"] == "full subtitle":
-                textlist = []
-                for ele in subtitle:
-                    pattern = r'<font color="white" size=".72c">(.*?)</font>'
-                    matches = re.findall(pattern, ele)
-                    if matches:
-                        textlist.append(matches[0])
-                subtitle_text = "\n".join(textlist)
-        else:
-            if "frame_num" in lmms_eval_specific_kwargs:
-                frame_num = lmms_eval_specific_kwargs["frame_num"]
-                subtitle_by_frame, total_frame = extract_subtitles(video_path, subtitle_path)
-                if frame_num == -1:
-                    frame_num = total_frame
-                uniform_sampled_frames = np.linspace(0, total_frame - 1, frame_num, dtype=int).tolist()
-
-                subtitle_by_frame_idx = []
-                for frame_idx in uniform_sampled_frames:
-                    for idx, title in enumerate(subtitle_by_frame):
-                        if frame_idx < title[1] and frame_idx >= title[0]:
-                            subtitle_by_frame_idx.append(idx)
-                subtitle_by_frame_idx = list(set(subtitle_by_frame_idx))
-
-                textlist = []
-                for idx in subtitle_by_frame_idx:
-                    pattern = r'<font color="white" size=".72c">(.*?)</font>'
-                    raw_text = re.findall(pattern, subtitle_by_frame[idx][2])
-                    try:
-                        textlist.append(raw_text[0])
-                    except:
-                        continue
-                subtitle_text = "\n".join(textlist)
-        subtitle = subtitle_text
-
-    option_prompt = "Select the best answer to the following multiple-choice question based on the video and the subtitles. Respond with only the letter (A, B, C, or D) of the correct option."
-    question = doc["question"]
-    option = "\n".join([f"{opt}" for i, opt in enumerate(doc["options"])])
-    question = question + "\n" + option
-    full_prompt = subtitles_prompt + subtitle + "\n" + option_prompt + "\n" + question + "\n" + "The best answer is:"
-    return full_prompt
 
 
 def extract_characters_regex(s):
@@ -168,38 +105,60 @@ def extract_characters_regex(s):
         return ""
     return matches[0]
 
+def extract_characters_regex(s):
+    s = s.strip()
+    answer_prefixes = [
+        "The best answer is",
+        "The correct answer is",
+        "The answer is",
+        "The answer",
+        "The best option is" "The correct option is",
+        "Best answer:" "Best option:",
+    ]
+    for answer_prefix in answer_prefixes:
+        s = s.replace(answer_prefix, "")
+
+    if len(s.split()) > 10 and not re.search("[ABCD]", s):
+        return ""
+
+    matches = re.search(r"[ABCD]", s)
+    if matches is None:
+        return ""
+    return matches[0]
 
 matrices = []
 
-for i in VIDEO_TYPE:
-    for j in CATEGORIES:
-        for k in SUB_CATEGORIES:
-            for l in TASK_CATEGORIES:
-                matrices.append(f"{i}_{j}_{k}_{l}")
+
+for k in LENGTH:
+    for l in TASK_CATEGORIES:
+        matrices.append(f"{str(k)}_{l}")
 
 
-def videomme_process_results(doc, results):
+def videoruler_process_results(doc, results):
     """
     Args:
         doc: a instance of the eval dataset
         results: [pred]
     Returns:
-        a dictionary with key: metric name (in this case videomme score), value: metric value
+        a dictionary with key: metric name (in this case videoruler score), value: metric value
     """
     pred = results[0]
-    pred_ans = extract_characters_regex(pred)
+    task_type = doc["type"]
+    if task_type=="QA":
+        pred_ans = extract_characters_regex(pred)
+    elif task_type=="OCR":
+        pred_ans = pred.strip()
     # gt_ans = doc["answer"].lower().strip().replace(".", "")
 
-    category = doc["domain"]
-    sub_category = doc["sub_category"]
-    task_category = doc["task_type"]
-    data_dict = {"question_id": doc["question_id"], "duration": doc["duration"], "category": category, "sub_category": sub_category, "task_category": task_category, "pred_answer": pred_ans, "answer": doc["answer"]}
+    
+    length=doc['length']
+    video=doc['video']
+    data_dict = {"index": doc["index"] ,"length":length,"video":video, "task_type": task_type, "pred_answer": pred_ans, "answer": doc["answer"]}
 
-    # return {f"videomme_perception_score": data_dict for metric in matrices}
-    return {f"videomme_perception_score": data_dict}
+    return {f"videoruler_perception_score": data_dict}
 
 
-def videomme_aggregate_results(results):
+def videoruler_aggregate_results(results):
     """
     Args:
         results: a list of values returned by process_results
@@ -208,58 +167,19 @@ def videomme_aggregate_results(results):
     """
     category2score = {}
 
-    for video_type in VIDEO_TYPE:
-        for category in CATEGORIES:
-            for sub_category in SUB_CATEGORIES:
-                for task_category in TASK_CATEGORIES:
-                    key = f"{video_type}_{category}_{sub_category}_{task_category}"
-                    category2score[key] = {"correct": 0, "answered": 0}
+    for k in LENGTH:
+        for l in TASK_CATEGORIES:
+            key = f"{str(k)}_{l}"
+            category2score[key] = {"correct": 0, "answered": 0}
 
     for result in results:
-        video_type = result["duration"]
-        category = result["category"]
-        sub_category = result["sub_category"]
-        task_category = result["task_category"]
-        key = f"{video_type}_{category}_{sub_category}_{task_category}"
+        task_type = result["task_type"]
+        length=result['length']
+        key = f"{length}_{task_type}"
         category2score[key]["answered"] += 1
         category2score[key]["correct"] += result["pred_answer"] == result["answer"]
 
-    for video_type in VIDEO_TYPE:
-        total_correct = 0
-        total_answered = 0
-        for k, v in category2score.items():
-            if video_type in k:
-                total_correct += v["correct"]
-                total_answered += v["answered"]
-        eval_logger.info(f"Evaluation on video Type: {video_type}: {100 * total_correct / total_answered if total_answered > 0 else 0 : .1f}%")
-
-    for category in CATEGORIES:
-        total_correct = 0
-        total_answered = 0
-        for k, v in category2score.items():
-            if category in k:
-                total_correct += v["correct"]
-                total_answered += v["answered"]
-        eval_logger.info(f"Evaluation on Categories: {category}: {100 * total_correct / total_answered if total_answered > 0 else 0 : .1f}%")
-
-    for sub_cate in SUB_CATEGORIES:
-        total_correct = 0
-        total_answered = 0
-        for k, v in category2score.items():
-            if sub_cate in k:
-                total_correct += v["correct"]
-                total_answered += v["answered"]
-        eval_logger.info(f"Evaluation on Video Sub Categories: {sub_cate}: {100 * total_correct / total_answered if total_answered > 0 else 0 : .1f}%")
-
-    for task_cate in TASK_CATEGORIES:
-        total_correct = 0
-        total_answered = 0
-        for k, v in category2score.items():
-            if task_cate in k:
-                total_correct += v["correct"]
-                total_answered += v["answered"]
-        eval_logger.info(f"Evaluation on Task Categories: {task_cate}: {100 * total_correct / total_answered if total_answered > 0 else 0 : .1f}%")
-
+    
     total_correct = 0
     total_answered = 0
     for k, v in category2score.items():
